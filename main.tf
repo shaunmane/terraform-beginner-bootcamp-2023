1a3c300d253edb1e9b85a5c37e37f8830b880d82
Shaun Mane <shaunmane.sm@gmail.com>
2024-10-14 10:20:48 +0000

    #48 finish implementation of terraform custom provider

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint  = "http://localhost:4567"
+  endpoint  = "http://localhost:4567/api"
   user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
   token     = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -36,18 +36,19 @@ provider "terratowns" {
 #   content_version = var.content_version
 #   assets_path = var.assets_path
 #}
-provider "terratowns" {
-  endpoint  = "http://localhost:4567"
-  user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
-  token     = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
-}
 
-#module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   content_version = var.content_version
-#   assets_path = var.assets_path
-#}
+resource "terratowns_home" "home" {
+  name = "Porsche Singer"
+  description = <<DESCRIPTION
+In June 2022, Singer unveiled the Turbo Study, 
+their first turbocharged model to celebrate the Porsche 930 Turbo based on a 964 chassis. 
+It features a 3.8L twin turbocharged, 
+intercooled flat-six producing either 450 in the standard trim or 510 HP in the sports focused trim.
+Power is sent through a 6-speed manual transmission and rear-wheel-drive drivetrain. 
+The suspension is touring-focused, and it features carbon-ceramic brakes.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
