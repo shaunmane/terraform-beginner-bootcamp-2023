a4229d319ea81e0b458b1172ea08e3842d1e5042
Shaun <131450123+shaunmane@users.noreply.github.com>
2024-11-09 12:20:30 +0200

    #50 test our terra home on terratowns test town (#51)

--- main.tf
+++ main.tf
@@ -22,20 +22,21 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint  = "http://localhost:4567/api"
-  user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
-  token     = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  # endpoint  = "https://localhost:4567/api"
+  endpoint  = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token     = var.terratowns_access_token 
 }
 
-#module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   content_version = var.content_version
-#   assets_path = var.assets_path
-#}
+module "terrahouse_aws" {
+   source = "./modules/terrahouse_aws"
+   user_uuid = var.teacherseat_user_uuid
+   #bucket_name = var.bucket_name
+   index_html_filepath = var.index_html_filepath
+   error_html_filepath = var.error_html_filepath
+   content_version = var.content_version
+   assets_path = var.assets_path
+}
 
 resource "terratowns_home" "home" {
   name = "Porsche Singer"
@@ -47,8 +48,8 @@ intercooled flat-six producing either 450 in the standard trim or 510 HP in the 
 Power is sent through a 6-speed manual transmission and rear-wheel-drive drivetrain. 
 The suspension is touring-focused, and it features carbon-ceramic brakes.
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "3fdq3gz.cloudfront.net"
+  town = "missingo"
   content_version = 1
 }
